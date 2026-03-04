788b54ae824b9473e7dc4010f6cff41d90954d51
Ali Selimovic <ali.selimovic11@gmail.com>
2026-03-04 18:47:40 +0100

    Initial project structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { AuctionsModule } from './auctions/auctions.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, AuthModule, AuctionsModule],
   controllers: [AppController],
   providers: [AppService],
 })
