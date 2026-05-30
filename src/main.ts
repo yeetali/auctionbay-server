0b56585f39ea427c230f20a63e1193ff0eab64ef
Ali Selimovic <ali.selimovic11@gmail.com>
2026-05-30 16:45:03 +0200

    modify cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({ origin: process.env.CORS_ORIGIN, credentials: true });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
